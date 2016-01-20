291b6de8b0275d4656f5610bf8ed6f1f1ce16595
mabre69uk <mabre69uk@gmail.com>
2016-01-20 11:44:09 +0000

    first commit

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -1,8 +1,8 @@
 public class JavaHelloWorld
 {
-		public static void main (String [] args)
-		{
-				System.out.printIn("Java Hello World");
-				System.out.printIn("Hello again");
-		}
+    public static void main (String [] args)
+    {
+        System.out.println ("Java Hello World")
+        System.out.println ("Hello again")
+    }
 }

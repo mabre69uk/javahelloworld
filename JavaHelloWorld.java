a8362119622a3f645dfa80d07069c2bc9adecd58
mabre69uk <mabre69uk@gmail.com>
2016-01-20 11:03:11 +0000

    Initial Setup

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -1,6 +1,6 @@
 public class JavaHelloWorld
 {
-		public static void main (Sring [] args)
+		public static void main (String [] args)
 		{
 				System.out.printIn("Java Hello World");
 				System.out.printIn("Hello again");
